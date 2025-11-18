2f281a8a4680baca5f426890db7e3159b1e3ebc4
Daan Eeltink <45791035+daan-eeltink@users.noreply.github.com>
2025-11-18 12:12:05 +0700

    Implemented Tokenization using SentencePiece

--- data.py
+++ data.py
@@ -2,7 +2,6 @@ import json
 from pathlib import Path
 import sentencepiece as spm
 
-
 # Function that parses JSONL file and returns list of text pairs
 def load_pairs_from_jsonl(jsonl_path: str):
     """
@@ -37,7 +36,50 @@ def load_pairs_from_jsonl(jsonl_path: str):
 
     return text_pairs
 
+# Train SentencePiece model on given set of text pairs
+def train_sentencepiece_model(texts, model_prefix, vocab_size=1000):
+    """
+    Train a SentencePiece model on given list of texts
+    """
+    
+    # Write all text pairs to a file (required input for SentencePiece)
+    tmp_file = Path(model_prefix + "_tmp.txt")
+    with open(tmp_file, "w", encoding="utf-8") as f:
+        for t in texts:
+            f.write(t.lower().strip() + "\n")
+    
+    # Learn the optimal tokens using SentencePiece model
+    spm.SentencePieceTrainer.train(
+        input=str(tmp_file),
+        model_prefix=model_prefix,
+        vocab_size=vocab_size,
+        character_coverage=1.0,
+        model_type="unigram",
+        pad_id=0, unk_id=1, bos_id=2, eos_id=3,
+    )
 
+    # Clean up the temporary file
+    tmp_file.unlink()
 
-path = r"C:\Users\daane\Desktop\GitHub Repos\NLP_Final_Project\dataset\train\de\train.jsonl"
-print(load_pairs_from_jsonl(path))
+# Load a SentencePiece model
+def load_sp_model(model_prefix):
+    sp = spm.SentencePieceProcessor()
+    sp.load(f"{model_prefix}.model")
+    return sp
+
+# Use SentencePiece model to tokenize text pairs
+def tokenize_text_pairs(text_pairs, sp_src, sp_tgt, max_src_len=128, max_tgt_len=128):
+    
+    # Initialize return objects
+    src_ids, tgt_ids = [], []
+
+    # Iterate over each text pair
+    for src, tgt in text_pairs:
+
+        # Use correct SentencePiece model to encode source or target text
+        src_toks = [2] + sp_src.encode(src.lower(), out_type=int)[:max_src_len-2] + [3]
+        tgt_toks = [2] + sp_tgt.encode(tgt.lower(), out_type=int)[:max_tgt_len-2] + [3]
+        src_ids.append(src_toks)
+        tgt_ids.append(tgt_toks)
+
+    return src_ids, tgt_ids
--- train.py
+++ train.py
@@ -1,1 +1,16 @@
 ### Training Script for NLP Final Project
+
+from data import load_pairs_from_jsonl, load_sp_model, tokenize_text_pairs, train_sentencepiece_model
+
+path = r"C:\Users\daane\Desktop\GitHub Repos\NLP_Final_Project\dataset\train\de\train.jsonl"
+
+text_pairs = load_pairs_from_jsonl(path)
+src_text = [s for s,t in text_pairs]
+tgt_text = [t for s,t in text_pairs]
+
+train_sentencepiece_model(src_text, "sp_en")
+train_sentencepiece_model(tgt_text, "sp_de")
+
+token_pairs = tokenize_text_pairs(text_pairs, load_sp_model("sp_en"), load_sp_model("sp_de"))
+
+print(token_pairs)
